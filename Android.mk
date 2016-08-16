6c847fe3669e3594e22f6b073ee3079119a6d377
Adam Lesinski <adamlesinski@google.com>
2016-08-16 15:59:14 -0700

    Build with AAPT2
    Change-Id: Ib0b4a597efae178785c308451b882196c2fb807a

--- Android.mk
+++ Android.mk
@@ -1,23 +1,18 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-appcompat_dir := ../../../prebuilts/sdk/current/support/v7/appcompat/res
-res_dir := res $(appcompat_dir)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_USE_AAPT2 := true
 
-LOCAL_STATIC_JAVA_LIBRARIES += \
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v13 \
     android-support-v4 \
     android-support-v7-appcompat
 
-LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
-LOCAL_SRC_FILES := $(call all-java-files-under, src/)
-
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
-
 LOCAL_PACKAGE_NAME := EVUpdater
+LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
